6739713672653e164b22d94f44eeaa9a36a3650d
Mickael Marques <mickaelpombal@gmail.com>
2019-12-10 23:09:25 +0000

    Moved the code for drawing the canvas to a different file so the index is purely the interface with the wasm code

---
+++ www/canvas.js
@@ -0,0 +1,144 @@
+import { Brick, GameStatus } from "wasm";
+
+const GRID_COLOR = "#CCCCCC";
+const DEAD_COLOR = "#FFFFFF";
+const ALIVE_COLOR = "#000000";
+
+const BACKGROUND_COLOUR = "#FFFFFF";
+const BORDER_COLOUR = "#333333";
+const MAIN_COLOUR = "#333333";
+
+export const init = ({
+  height,
+  width,
+  getPadPosition,
+  getBallPosition,
+  getBricksData,
+  getGameStatus,
+  tick,
+  movePad,
+  startGame
+}) => {
+  document.addEventListener(
+    "keypress",
+    event => {
+      if (event.key === "h") {
+        movePad(false);
+      } else if (event.key === "l") {
+        movePad(true);
+      } else if (event.key === "k") {
+        startGame();
+      }
+    },
+    false
+  );
+
+  const canvas = document.getElementById("game");
+  canvas.height = height;
+  canvas.width = width;
+
+  const ctx = canvas.getContext("2d");
+
+  const clearPanel = () => {
+    ctx.beginPath();
+    ctx.fillStyle = BACKGROUND_COLOUR;
+    ctx.fillRect(0, 0, width, height);
+    ctx.stroke();
+  };
+
+  const drawPad = () => {
+    const { top, left, height, width } = getPadPosition();
+
+    ctx.beginPath();
+    ctx.fillStyle = MAIN_COLOUR;
+    ctx.fillRect(left, top, width, height);
+    ctx.stroke();
+  };
+
+  const drawBall = () => {
+    const { x, y, radius } = getBallPosition();
+
+    ctx.beginPath();
+    ctx.fillStyle = MAIN_COLOUR;
+    ctx.arc(x, y, radius, 0, Math.PI * 2, true);
+    ctx.fill();
+  };
+
+  const drawBricks = () => {
+    const {
+      rowCount,
+      colCount,
+      width,
+      height,
+      bricksPtr,
+      marginHeight,
+      marginWidth,
+      bricks
+    } = getBricksData();
+
+    ctx.beginPath();
+    ctx.fillStyle = MAIN_COLOUR;
+    for (let row = 0; row < rowCount; row++) {
+      for (let col = 0; col < colCount; col++) {
+        if (bricks[row * colCount + col] === Brick.Alive) {
+          ctx.fillRect(
+            marginWidth + 1 + width * col,
+            marginHeight + 1 + height * row,
+            width - 2,
+            height - 2
+          );
+        }
+      }
+    }
+    ctx.stroke();
+  };
+
+  let animationId = null;
+
+  const isPaused = () => {
+    return animationId === null;
+  };
+
+  const renderLoop = () => {
+    const gameStatus = getGameStatus();
+    if (gameStatus === GameStatus.Lost || gameStatus === GameStatus.Won) {
+      const overlay = document.getElementById("overlay");
+      overlay.style.visibility = "visible";
+      const header = document.getElementById("header");
+      header.textContent =
+        gameStatus === GameStatus.Lost ? "Loser!!!" : "Winner!!!";
+    }
+
+    clearPanel();
+    drawPad();
+    drawBall();
+    drawBricks();
+
+    tick();
+
+    animationId = requestAnimationFrame(renderLoop);
+  };
+
+  const playPauseButton = document.getElementById("play-pause");
+
+  const play = () => {
+    playPauseButton.textContent = "⏸";
+    renderLoop();
+  };
+
+  const pause = () => {
+    playPauseButton.textContent = "▶";
+    cancelAnimationFrame(animationId);
+    animationId = null;
+  };
+
+  playPauseButton.addEventListener("click", event => {
+    if (isPaused()) {
+      play();
+    } else {
+      pause();
+    }
+  });
+
+  play();
+};
--- www/index.js
+++ www/index.js
@@ -1,147 +1,59 @@
 import { Universe, Brick, GameStatus } from "wasm";
 import { memory, universe_pad_height } from "wasm/wasm_bg";
-
-const GRID_COLOR = "#CCCCCC";
-const DEAD_COLOR = "#FFFFFF";
-const ALIVE_COLOR = "#000000";
-
-const BACKGROUND_COLOUR = "#FFFFFF";
-const BORDER_COLOUR = "#333333";
-const MAIN_COLOUR = "#333333";
+import { init } from "./canvas";
 
 // Construct the universe, and get its width and height.
 const universe = Universe.new();
 const width = universe.width();
 const height = universe.height();
 
-// Give the canvas room for all of our cells and a 1px border
-// around each of them.
-const canvas = document.getElementById("game");
-canvas.height = height;
-canvas.width = width;
+const movePad = right => universe.move_pad(right);
+const startGame = () => universe.start_ball();
 
-document.addEventListener(
-  "keypress",
-  event => {
-    if (event.key === "h") {
-      universe.move_pad(false);
-    } else if (event.key === "l") {
-      universe.move_pad(true);
-    } else if (event.key === "k") {
-      universe.start_ball();
-    }
-  },
-  false
-);
-
-const ctx = canvas.getContext("2d");
-
-const getIndex = (row, column) => {
-  return row * width + column;
-};
-
-const clearPanel = () => {
-  ctx.beginPath();
-  ctx.fillStyle = BACKGROUND_COLOUR;
-  ctx.fillRect(0, 0, width, height);
-  ctx.stroke();
-};
-
-const drawPad = () => {
-  const top = universe.pad_top_position();
-  const left = universe.pad_left_position();
-  const height = universe.pad_height();
-  const width = universe.pad_width();
-
-  ctx.beginPath();
-  ctx.fillStyle = MAIN_COLOUR;
-  ctx.fillRect(left, top, width, height);
-  ctx.stroke();
-};
-
-const drawBall = () => {
-  const x = universe.ball_x_position();
-  const y = universe.ball_y_position();
-  const radius = universe.ball_radius();
-
-  ctx.beginPath();
-  ctx.fillStyle = MAIN_COLOUR;
-  ctx.arc(x, y, radius, 0, Math.PI * 2, true);
-  ctx.fill();
-};
-
-const drawBricks = () => {
-  const rowCount = universe.row_count();
-  const colCount = universe.column_count();
-  const width = universe.brick_width();
-  const height = universe.brick_height();
-  const bricksPtr = universe.bricks();
-  const marginHeight = universe.margin_height();
-  const marginWidth = universe.margin_width();
-  const bricks = new Uint8Array(memory.buffer, bricksPtr, rowCount * colCount);
-
-  ctx.beginPath();
-  ctx.fillStyle = MAIN_COLOUR;
-  for (let row = 0; row < rowCount; row++) {
-    for (let col = 0; col < colCount; col++) {
-      if (bricks[row * colCount + col] === Brick.Alive) {
-        ctx.fillRect(
-          marginWidth + 1 + width * col,
-          marginHeight + 1 + height * row,
-          width - 2,
-          height - 2
-        );
-      }
-    }
-  }
-  ctx.stroke();
-};
-
-let animationId = null;
-
-const isPaused = () => {
-  return animationId === null;
-};
-
-const renderLoop = () => {
-  const gameStatus = universe.get_status();
-  if (gameStatus === GameStatus.Lost || gameStatus === GameStatus.Won) {
-    const overlay = document.getElementById("overlay");
-    overlay.style.visibility = "visible";
-    const header = document.getElementById("header");
-    header.textContent =
-      gameStatus === GameStatus.Lost ? "Loser!!!" : "Winner!!!";
-  }
-
-  clearPanel();
-  drawPad();
-  drawBall();
-  drawBricks();
-
-  universe.tick();
-
-  animationId = requestAnimationFrame(renderLoop);
-};
-
-const playPauseButton = document.getElementById("play-pause");
-
-const play = () => {
-  playPauseButton.textContent = "⏸";
-  renderLoop();
-};
-
-const pause = () => {
-  playPauseButton.textContent = "▶";
-  cancelAnimationFrame(animationId);
-  animationId = null;
-};
-
-playPauseButton.addEventListener("click", event => {
-  if (isPaused()) {
-    play();
-  } else {
-    pause();
-  }
+const getPadPosition = () => ({
+  top: universe.pad_top_position(),
+  left: universe.pad_left_position(),
+  height: universe.pad_height(),
+  width: universe.pad_width()
 });
 
-play();
+const getBallPosition = () => ({
+  x: universe.ball_x_position(),
+  y: universe.ball_y_position(),
+  radius: universe.ball_radius()
+});
+
+const getBricksData = () => {
+  const rowCount = universe.row_count();
+  const colCount = universe.column_count();
+  const bricksPtr = universe.bricks();
+
+  return {
+    rowCount,
+    colCount,
+    width: universe.brick_width(),
+    height: universe.brick_height(),
+    bricksPtr,
+    marginHeight: universe.margin_height(),
+    marginWidth: universe.margin_width(),
+    bricks: new Uint8Array(memory.buffer, bricksPtr, rowCount * colCount)
+  };
+};
+
+const getGameStatus = () => universe.get_status();
+
+const tick = () => {
+  universe.tick();
+};
+
+init({
+  height,
+  width,
+  getPadPosition,
+  getBallPosition,
+  getBricksData,
+  getGameStatus,
+  tick,
+  movePad,
+  startGame
+});
