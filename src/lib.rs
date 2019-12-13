6c7314bd05d219e9ad08ad2a2ca5195b21993922
Mickael Marques <mickaelpombal@gmail.com>
2019-12-13 18:56:45 +0000

    Removed commented code

--- src/lib.rs
+++ src/lib.rs
@@ -181,37 +181,6 @@ impl Universe {
                 self.status = GameStatus::Lost
             }
         }
-
-        // let mut next = self.cells.clone();
-
-        // for row in 0..self.height {
-        //     for col in 0..self.width {
-        //         let idx = self.get_index(row, col);
-        //         let cell = self.cells[idx];
-        //         let live_neighbors = self.live_neighbor_count(row, col);
-
-        //         let next_cell = match (cell, live_neighbors) {
-        //             // Rule 1: Any live cell with fewer than two live neighbours
-        //             // dies, as if caused by underpopulation.
-        //             (Cell::Alive, x) if x < 2 => Cell::Dead,
-        //             // Rule 2: Any live cell with two or three live neighbours
-        //             // lives on to the next generation.
-        //             (Cell::Alive, 2) | (Cell::Alive, 3) => Cell::Alive,
-        //             // Rule 3: Any live cell with more than three live
-        //             // neighbours dies, as if by overpopulation.
-        //             (Cell::Alive, x) if x > 3 => Cell::Dead,
-        //             // Rule 4: Any dead cell with exactly three live neighbours
-        //             // becomes a live cell, as if by reproduction.
-        //             (Cell::Dead, 3) => Cell::Alive,
-        //             // All other cells remain in the same state.
-        //             (otherwise, _) => otherwise,
-        //         };
-
-        //         next[idx] = next_cell;
-        //     }
-        // }
-
-        // self.cells = next;
     }
 
     fn check_brick_colision(&self, ball_x: u32, ball_y: u32, index: u32) -> u8 {
@@ -254,27 +223,6 @@ impl Universe {
         }
         BrickColision::No as u8
     }
-
-    // fn get_index(&self, row: u32, column: u32) -> usize {
-    //     (row * self.width + column) as usize
-    // }
-
-    // fn live_neighbor_count(&self, row: u32, column: u32) -> u8 {
-    //     let mut count = 0;
-    //     for delta_row in [self.height - 1, 0, 1].iter().cloned() {
-    //         for delta_col in [self.width - 1, 0, 1].iter().cloned() {
-    //             if delta_row == 0 && delta_col == 0 {
-    //                 continue;
-    //             }
-
-    //             let neighbor_row = (row + delta_row) % self.height;
-    //             let neighbor_col = (column + delta_col) % self.width;
-    //             let idx = self.get_index(neighbor_row, neighbor_col);
-    //             count += self.cells[idx] as u8;
-    //         }
-    //     }
-    //     count
-    // }
 
     pub fn new() -> Universe {
         let width = WIDTH;
@@ -384,11 +332,6 @@ impl Universe {
     pub fn margin_width(&self) -> u32 {
         MARGIN_WIDTH
     }
-
-    // pub fn toggle_cell(&mut self, row: u32, column: u32) {
-    //     let idx = self.get_index(row, column);
-    //     self.cells[idx].toggle();
-    // }
 
     pub fn ball_x_position(&self) -> u32 {
         self.ball.x
